88e0e5fc2f555a47c57acffabff3f6f348550653
Dillon Castelluccio <dilloncas@hotmail.com>
2022-09-12 14:33:35 -0400

    update

--- addremove.js
+++ addremove.js
@@ -10,10 +10,10 @@ const searchEngine = {
     state: ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 }
 
-const addressInput = document.querySelector(".street-address");
-const propertyInput = document.querySelector(".property-type");
-const cityInput = document.querySelector(".city");
-const stateInput = document.querySelector(".state");
+const addressInput = document.querySelector(".street-address"); //address searchbar
+const propertyInput = document.querySelector(".property-type"); //property dropdown
+const cityInput = document.querySelector(".city"); //city searchbar
+const stateInput = document.querySelector(".state"); //state dropdown
 
 //Submit Button
 const submitButton = document.querySelector(".submit");
