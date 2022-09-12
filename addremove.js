ebbf0ef310c310f0d5450892849ac683e3d0a71b
Dillon Castelluccio <dilloncas@hotmail.com>
2022-09-12 15:19:15 -0400

    added js file for class

--- addremove.js
+++ addremove.js
@@ -3,12 +3,12 @@
 //Can enter the street address, property type (house, apartment, loft, etc.), city, and state into `input` elements`.
 
 
-const searchData = {
-    address: ["123 Main Street"],
-    propertyType: ["house", "apartment", "condo"],
-    city: ["Anytown"],
-    state: ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-}
+// const searchData = {
+//     address: ["123 Main Street"],
+//     propertyType: ["house", "apartment", "condo"],
+//     city: ["Anytown"],
+//     state: ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+// }
 
 const address = document.querySelector(".street-address"); //address searchbar
 const property = document.querySelector(".property-type"); //property dropdown
@@ -19,7 +19,9 @@ const state = document.querySelector(".state"); //state dropdown
 const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", function() {
     let addressInput = document.getElementsByClassName(".street-address").value;
-    return ;
+    console.log(searchData.filter(filterAddress));
+        function filterAddress(address) {
+        }
 //submit button needs to add items to array from all inputs
 });
 
